08e2b6b0ed6f52a5620c57ad66f77c6c9397f850
zaScripts <rune38@gmail.com>
2014-01-25 15:39:26 +0200

    Update RetrieveFalcon.java

--- task/tasks/RetrieveFalcon.java
+++ task/tasks/RetrieveFalcon.java
@@ -47,9 +47,7 @@ public class RetrieveFalcon extends Task
 		
 			}else //if falcon is not on the screen
 			{
-				ctx.camera.turnTo(falcon.getLocation());
-				if(falcon.isOnScreen())
-					return;
+
 				ctx.movement.stepTowards(falcon.getLocation());
 				Condition.wait(new Callable <Boolean>()
 						{
